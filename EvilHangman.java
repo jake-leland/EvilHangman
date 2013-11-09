6415874630ffed321998728ea84c4b53a83c7fa4
Jake Leland <jake-leland@users.noreply.github.com>
2013-11-08 22:07:51 -0600

    Update EvilHangman.java

--- EvilHangman.java
+++ EvilHangman.java
@@ -4,7 +4,7 @@
  * by Jake Leland
  *
  * This game is an "evil" version of hangman. It functions very similarly to the hangman game that everyone is used to, but it is much more difficult.
- * Instead of picking a word at the beginning of the game, the computer never decides on an actual word until the very end.
+ * Rather than picking a word at the beginning of the game, the computer never decides on an actual word until the very end.
  * Instead, it changes the "word" that the user is guessing so that it is as difficult as possible to actually guess.
  * As the user guesses letters, the code goes through a dictionary and determines if there are more words with or without the guessed letter.
  * If there are more words in the dictionary without the guessed letter, then the computer will say that the word does not contain the letter, because this leaves more words as the possible answer.
